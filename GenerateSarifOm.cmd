fc60a9fb8503bb12a51e65c5de0fb170a5f4f8b2
Michael Scovetta <michael.scovetta@microsoft.com>
2020-11-18 22:13:10 -0800

    Bump reference in generate cmd

--- GenerateSarifOm.cmd
+++ GenerateSarifOm.cmd
@@ -1,6 +1,6 @@
 @echo off
 py -m jschema_to_python ^
-    --schema-path sarif-2.1.0-rtm.5.json ^
+    --schema-path sarif-schema-2.1.0.json ^
     --module-name sarif_om ^
     --output-directory sarif_om ^
     --root-class-name SarifLog ^
